56d19784976bc2576c5c05722e69bc57ccf6de25
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-12-24 09:37:44 +0100

    apply small changes

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -6,14 +6,14 @@ import FeaturedInfo from '../featuredInfo/FeaturedInfo';
 
 const Home = () => {
   return (
-    <section className="home">
+    <>
       <FeaturedInfo />
       <Chart title="User Analytics" aspect={3 / 1} />
       <div className="homeWidgets">
         <WidgetSm />
         <WidgetLg />
       </div>
-    </section>
+    </>
   );
 };
 export default Home;
